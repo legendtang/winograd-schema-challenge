cc56dc5cdae3014303475d7b202d7fb78a3904e7
Wensheng Tang <sp3478@gmail.com>
2017-11-18 04:46:39 +0800

    Refactoring and docs

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# winograd-schema-challenge
-A solver trying to cover winograd schema challenge problem
+# Introduction
+A solver trying to cover winograd schema challenge problem.
+
+# Instruction
+
+Install spacy:
+
+```
+pip install spacy-nightly
+python -m spacy download en_core_web_sm-2.0.0-alpha --direct   # English
+```
+
+Please make sure that you have enough permission to install packages. If not, please install in `virtualenv` instead.
+
+Run the program:
+
+```
+python main.py
+```
+
+It will shows the statistic information at the end of output.
+
+You may also find help by using `-h`:
+```
+python main.py -h
+
+usage: main.1.py [-h] [-i INPUT] [-f FORCE] [-n NLP]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -i INPUT, --input INPUT
+                        Input dataset: 1 for WSC2016 and 2 for WSC Example
+  -f FORCE, --force FORCE
+                        Force making decisions on blank
+  -n NLP, --nlp NLP    Output NLP properties
+```
--- main.py
+++ main.py
@@ -11,18 +11,27 @@ import sys
 import xml.etree.cElementTree as ET
 from spacy.symbols import *
 
+import argparse
+
+# Create ArgumentParser() object
+parser = argparse.ArgumentParser()
+
+# Add argument
+parser.add_argument('-i', '--input', default=1, type=int, required=False, help='Input dataset: 1 for WSC2016 and 2 for WSC Example')
+parser.add_argument('-f', '--force', default=0, type=int,required=False, help='Force making decisions on blank')
+parser.add_argument('-n', '--nlp', default=0, type=int, required=False, help='Output NLP properties')
+
+# Print usage
+parser.print_help()
+
+# Parse argument
+args = parser.parse_args()
+
 nlp = spacy.load('en')
-
-# doc1 = nlp(u'Text I do want parsed.')
-
-# doc = nlp(u'London is a big city in the United Kingdom.')
-# print(doc[0].text, doc[0].ent_iob, doc[0].ent_type_)
-# # (u'London', 2, u'GPE')
-# print(doc[1].text, doc[1].ent_iob, doc[1].ent_type_)
-# # (u'is', 3, u'')
-
-# tree = ET.ElementTree(file='./WSCollection.xml')
-tree = ET.ElementTree(file='./PDPChallenge.xml')
+if args.input != 1:
+    tree = ET.ElementTree(file='./WSCollection.xml')
+else:
+    tree = ET.ElementTree(file='./PDPChallenge.xml')
 root = tree.getroot()
 
 
@@ -35,6 +44,7 @@ def resolve_index_to_answer(idx):
         return "C"
     else:
         return "D"
+
 
 def print_statistic():
     # simple statistic
@@ -57,7 +67,6 @@ def print_statistic():
     print "Wrong Answer List:", res_ans
     print "Correct Rate:", str(100 * correct_count / len(root)) + "%"
     print "Wrong Rate:", str(100 * wrong_count / len(root)) + "%"
-
 
 
 answer_list = [None] * len(root)
@@ -100,7 +109,7 @@ for cur, child_of_root in enumerate(root):
 
                 if grandgrandchild_of_root == grandchild_of_root[-1]:
                     sentence = (text1 + target + text2).strip().replace('\n',
-                                                                        '').replace('\r', '').replace('  ', ' ')
+                                                                        ' ').replace('\r', '').replace('  ', ' ')
                     target = target.strip()
                     index = len(text1.split())
                     # print(index)
@@ -126,13 +135,17 @@ for cur, child_of_root in enumerate(root):
 
         # process text
         if grandchild_of_root == child_of_root[-1]:
-            print(sentence)
+            sentence = sentence.replace('  ', ' ')
+            print sentence
             document = nlp(sentence.decode('utf8'))
+
+            if args.nlp == 1:
+                for token in document:
+                    print(token.text, token.pos_, token.dep_)
             sentence_list = list(document.sents)
 
             # try to detect in which sentence
             which_sentence = 0
-            # for
 
             verbs_pre = []
             prps_pre = []
@@ -140,7 +153,7 @@ for cur, child_of_root in enumerate(root):
             prps_post = {}
             trunk = collections.OrderedDict()
             pns = []
-            adjpns = {}  # second num refers to pos in pns
+            adjpns = []  # second num refers to pos in pns
             despns = {}
 
             print(quote)
@@ -165,17 +178,17 @@ for cur, child_of_root in enumerate(root):
                         if (quote_tk.text == target and
                                 (quote_doc[quote_tk.i - 1].text == document[token.i - 1].text or
                                  (quote_tk.i + 1 < len(quote_doc) and token.i + 1 < len(document)
-                                  and quote_doc[quote_tk.i + 1].text == document[token.i + 1].text)
+                                          and quote_doc[quote_tk.i + 1].text == document[token.i + 1].text)
                                  )
-                            ):
+                                ):
                             if quote_tk.i + 2 < len(quote_doc) and token.i + 2 < len(document):
                                 if quote_doc[quote_tk.i + 2].text == document[token.i + 2].text:
                                     word_index = token.i
-                                    print token.i, target
+                                    # print token.i, target
                                     count_temp = count_temp + 1
                             else:
                                 word_index = token.i
-                                print token.i, target
+                                # print token.i, target
                                 count_temp = count_temp + 1
 
             # print(sentence.find(quote) + quote.find(target))
@@ -184,78 +197,105 @@ for cur, child_of_root in enumerate(root):
             # constructtrunk and additional properties
             for possible_subject in document:
                 if possible_subject.head.pos == VERB:
-                    if not trunk.has_key(possible_subject.head):
-                        trunk[possible_subject.head] = [None, None]
+                    # if not trunk.has_key(possible_subject.head):
+                    #     trunk[possible_subject.head] = [None] * 4
                     if possible_subject.dep == nsubj:
-                        trunk[possible_subject.head][0] = {
-                            possible_subject: possible_subject.i}
+                        if not trunk.has_key(possible_subject.head):
+                            trunk[possible_subject.head] = [None] * 4
+                        trunk[possible_subject.head][0] = possible_subject
+                        trunk[possible_subject.head][1] = possible_subject.i
                         for possible_pn in possible_subject.children:
                             if possible_pn.dep == poss and possible_pn.pos == ADJ:
                                 # print possible_pn.text, possible_subject.text
-                                adjpns[possible_pn.text] = possible_subject.text
+                                # adjpns[possible_pn.text] = possible_subject.text
+                                adjpns.append([possible_pn.text, possible_pn.i, possible_subject.text, possible_subject.i])
+                                # adjpns[possible_pn.text] = possible_pn.i
                             elif possible_pn.dep == appos and possible_pn.pos == PROPN:
                                 # print possible_pn.text, possible_subject.text
                                 despns[possible_pn.text] = possible_subject.text
                     elif possible_subject.dep == dobj and (possible_subject.pos == PRON or possible_subject.pos == NOUN):
-                        trunk[possible_subject.head][1] = {
-                            possible_subject: possible_subject.i}
+                        if not trunk.has_key(possible_subject.head):
+                            trunk[possible_subject.head] = [None] * 4
+                        trunk[possible_subject.head][2] = possible_subject
+                        trunk[possible_subject.head][3] = possible_subject.i
                 if possible_subject.head.pos == NOUN and possible_subject.dep == poss:
                     if possible_subject.pos == ADJ:
-                        adjpns[possible_subject] = possible_subject.head
+                        # adjpns[possible_subject] = possible_subject.head
+                        adjpns.append([possible_subject.text, possible_subject.i, possible_subject.head.text, possible_subject.head.i])
+                        # adjpns[possible_subject] = possible_subject.i
 
             print trunk
             print adjpns
             print despns
 
             for i, (verb, pn) in enumerate(trunk.iteritems()):
-                if pn[0] is not None and word_index in pn[0].itervalues():
+                if pn[0] is not None and word_index == pn[1]:
                     """ Pronouns as subjects """
                     for num in reversed(xrange(i)):
-                        if trunk.items()[num] is not None:
+                        cur_trunk = trunk.items()[num]
+                        if cur_trunk is not None:
                             for j, ans in enumerate(choices[cur]):
                                 # print j, trunk.items()[num][1][0], ans, 88888888888888
-                                if trunk.items()[num][1][0] is not None:
-                                    for key in trunk.items()[num][1][0].iterkeys():
-                                        if not ans.find(key.text):
-                                            answer_list[cur] = resolve_index_to_answer(
-                                                j)
-                                        elif len(choices[cur]) == 2:
-                                            print 4444444444444444
-                                            answer_list[cur] = resolve_index_to_answer(
-                                                1 - j)
+                                if cur_trunk[1][0] is not None:
+                                    key = cur_trunk[1][0]
+                                    # print key, ans, 999999999
+                                    if ans.find(key.text) >= 0 and answer_list[cur] is None:
+                                        # print "yes!"
+                                        answer_list[cur] = resolve_index_to_answer(j)
+                                    elif len(choices[cur]) == 2:
+                                        # print 4444444444444444
+                                        answer_list[cur] = resolve_index_to_answer(1 - j)
 
-                elif pn[1] is not None and word_index in pn[1].itervalues():
+                elif pn[1] is not None and word_index == pn[3]:
                     """ Pronouns as objects """
                     last_idx = -1
                     for num in reversed(xrange(i)):
-                        if trunk.items()[num] is not None:
+                        cur_trunk = trunk.items()[num]
+                        if cur_trunk is not None:
                             if last_idx < 0:
                                 last_idx = num
                             for j, ans in enumerate(choices[cur]):
                                 # print j, trunk.items()[num][1][0], ans, 88888888888888
-                                if trunk.items()[num][1][1] is not None:
-                                    for key in trunk.items()[num][1][1].iterkeys():
-                                        if not ans.find(key.text):
-                                            print len(choices[cur])
-                                            answer_list[cur] = resolve_index_to_answer(
-                                                j)
-                                            continue
-                                        elif len(choices[cur]) == 2:
-                                            print 4444444444444444
-                                            continue
-                                            # Don't add that num otherwise more FP
-                                            # answer_list[cur] = resolve_index_to_answer(1 - j)
-                        if trunk.items()[0][1][1] is None:
+                                if cur_trunk[1][2] is not None:
+                                    key = cur_trunk[1][2]
+                                    if ans.find(key.text) > 0:
+                                        # print len(choices[cur])
+                                        answer_list[cur] = resolve_index_to_answer(j)
+                                        continue
+                                    elif len(choices[cur]) == 2:
+                                        # Don't add that num otherwise more FP
+                                        answer_list[cur] = resolve_index_to_answer(1 - j)
+                                        continue
+                        if trunk.items()[0][1][2] is None:
                             for j, ans in enumerate(choices[cur]):
-                                print trunk.items()[last_idx][1][0]
-                                for key in trunk.items()[last_idx][1][0].iterkeys():
-                                    if ans.find(key.text):
-                                        answer_list[cur] = resolve_index_to_answer(
-                                                    j)
+                                # print last_idx, j, ans
+                                # print trunk.items()[last_idx][1][0]
+                                key = trunk.items()[last_idx][1][0]
+                                # print key
+                                if key is not None and ans.find(key.text) and len(choices[cur]) == 2:
+                                    answer_list[cur] = resolve_index_to_answer(j)
 
-            if choices[cur] == None:
+            if answer_list[cur] == None:
                 for adj in adjpns:
-                    print adj
+                    if adj[1] == word_index:
+                        for i, (verb, pn) in enumerate(trunk.iteritems()):
+                            if adj[3] == pn[1]:
+                                for num in reversed(xrange(i)):
+                                    cur_trunk = trunk.items()[num]
+                                    for j, ans in enumerate(choices[cur]):
+                                        # print adj[2] == cur_trunk[1][0].text
+                                        if cur_trunk[1][0] is not None and adj[2] != cur_trunk[1][0].text:
+                                            if ans.find(cur_trunk[1][0].text) < 0:
+                                                print len(choices[cur])
+                                                answer_list[cur] = resolve_index_to_answer(j)
+                                                break
+                                            elif len(choices[cur]) == 2:
+                                                answer_list[cur] = resolve_index_to_answer(1 - j)
+                                                break
+                
+            if args.force == 1 and answer_list[cur] == None:
+                answer_list[cur] = 'A'
+
 
                     # elif
                     # if possible_subject.dep == nsubj and possible_subject.head.pos == VERB:
@@ -277,8 +317,7 @@ for cur, child_of_root in enumerate(root):
                     # print(verbs_post)
                     # print(prps_post)
 
-    # if child_of_root == root[-1]:
-    if cur == 4:
+    if child_of_root == root[-1]:
+    # if cur == 4:
         print_statistic()
         exit(0)
-
